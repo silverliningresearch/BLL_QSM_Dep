7ebbe167783eec8a5eca0c40b2a4ea28febecab9
lexuanbinh <lb@silverliningresearch.com>
2023-11-07 11:17:04 +0700

    update new files

--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -17,7 +17,7 @@ var total_completed_percent;
 
 var total_quota_completed;
 var total_hard_quota;
-var Oct_2023_cut_off_day = 10;
+var Oct_2023_cut_off_day = 7;
 
 /************************************/
 function initCurrentTimeVars() {
